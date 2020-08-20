bc3b2944186d05001d72af80ff7c00c260af91cc
Olajide Awonusi <oawonusi@hq.icsoutsourcing.com>
2020-08-20 12:23:06 +0100

    first commit

--- storage/framework/views/ba29550c0262fdbf969f320c90e82b4f76dd0217.php
+++ storage/framework/views/ba29550c0262fdbf969f320c90e82b4f76dd0217.php
@@ -75,7 +75,7 @@
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block btn-flat">xxSign In</button>
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
           </div>
           <!-- /.col -->
         </div>
